1304b5c9b8b1a8815a65319325b8c07461719d50
Elia <elahehahmadiehyazdi@gmail.comm>
2019-05-23 09:41:08 +0200

    update

---
+++ Js1/Week02/4vary.js
@@ -0,0 +1,6 @@
+let y = "bacheha salam"
+console.log ("the value of my string is : bacheha salam")
+console.log (y)
+let y = "bacheha bye"
+console.log ("the value of my string is : bacheha bye")
+console.log (y)
