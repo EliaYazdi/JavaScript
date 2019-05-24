f110bf5e5322265c8f1fb02813ea3bd390495263
Elia <elahehahmadiehyazdi@gmail.comm>
2019-05-24 12:06:20 +0200

    Done!

---
+++ 10.lastassignment.js
@@ -0,0 +1,5 @@
+let myArray = [42 , "Elia" ,100 , "Rana"];
+console.log ("In this array we have both numbers and strings!")
+//comparing infinities
+console.log( 6/0===10/0 )
+console.log (" when we compare two infinities togerher they are equal!")
--- Js1/Week02/9remainder.js
+++ Js1/Week02/9remainder.js
@@ -5,3 +5,7 @@ console.log ("the new value of x will be 1 because x=7 is a odd number")
 console.log ("two 3s will fit into 7 and then 1 will remain extra!")
 Math.floor (x/3);
 console.log ("the new value of x is equal to: " + x + "");
+//or
+let w = 7;
+w %= 3;
+console.log (w);
