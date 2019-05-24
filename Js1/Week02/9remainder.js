b81124b1ceb9d964e6f465b8f15bc699d3ecbf20
Elia <elahehahmadiehyazdi@gmail.comm>
2019-05-24 11:40:17 +0200

    Done!

---
+++ Js1/Week02/9remainder.js
@@ -0,0 +1,7 @@
+let x = 7;
+x = x % 3;
+console.log ("the % sign shows the remainder of a division.")
+console.log ("the new value of x will be 1 because x=7 is a odd number")
+console.log ("two 3s will fit into 7 and then 1 will remain extra!")
+Math.floor (x/3);
+console.log ("the new value of x is equal to: " + x + "");
