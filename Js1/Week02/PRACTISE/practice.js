b18dbbcfaae42f8a8fffcf279e6bc011caa7f9ab
Elia <elahehahmadiehyazdi@gmail.comm>
2019-05-26 22:04:26 +0200

    update

---
+++ Js1/Week02/PRACTISE/practice.js
@@ -0,0 +1,19 @@
+console.log('Hello World!');
+let firstName ='Elia'; //considered more modern to use single quote!
+console.log (firstName);
+let lastName ='yazdi';
+let age = 28;
+let fullAge = true;
+console.log (fullAge);
+let job;
+console.log (job);
+job = 'teacher'; // we can go ahead and later add(assign) a value to an undefined variable.
+console.log (job);
+
+//let 3years; // we cannot start a variable name with a number
+// we can start it with _ underscore or $ dollar sign
+let $3years;
+
+// let elia/yazdi; // no symbols in the midlle is allowed
+// we use camel case to distinguish between the words and know when the new word starts
+//let function; we cannot use reserved JS words as well
